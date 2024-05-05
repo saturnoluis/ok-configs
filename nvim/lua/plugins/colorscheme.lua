3804008412e8c53fd680cdebd0649582e8e34d77
Luis Saturno <saturno.luis@gmail.com>
2024-05-05 17:05:33 +0200

    Use gruvbox as theme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,9 @@
 return {
-	"nyoom-engineering/oxocarbon.nvim",
+	"ellisonleao/gruvbox.nvim",
 
 	lazy = false,
 	priority = 1000,
 	config = function()
-		vim.cmd([[colorscheme oxocarbon]])
+		vim.cmd([[colorscheme gruvbox]])
 	end,
 }
