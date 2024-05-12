2dbf90788481a7fd5d903209044d89afde59c922
Luis Saturno <saturno.luis@gmail.com>
2024-05-12 21:51:54 +0200

    foo

--- nvim/lua/config/editor.lua
+++ nvim/lua/config/editor.lua
@@ -1,3 +1,8 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.list = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = false
--- nvim/lua/config/plugins.lua
+++ nvim/lua/config/plugins.lua
@@ -1,13 +1,16 @@
+-- Bootstraps the lazy.nvim plugin manager if it's not already installed,
+-- then loads the user's plugin configuration.
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
