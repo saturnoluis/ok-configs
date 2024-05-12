26a8f9c0cbe0bfb9bd2164ebfc3669520174715f
Luis Saturno <saturno.luis@gmail.com>
2024-05-12 19:43:11 +0200

    Add nvim-treesitter for syntax highlight

---
+++ nvim/lua/plugins/tree-sitter.lua
@@ -0,0 +1,30 @@
+return {
+	"nvim-treesitter/nvim-treesitter",
+
+	build = ":TSUpdate",
+	event = "VeryLazy",
+	main = "nvim-treesitter.configs",
+	opts = {
+		ensure_installed = {
+			"bash",
+			"css",
+			"html",
+			"javascript",
+			"jsdoc",
+			"json",
+			"lua",
+			"luadoc",
+			"markdown",
+			"python",
+			"scss",
+			"svelte",
+			"typescrypt",
+		},
+		highlight = {
+			enable = true,
+		},
+		indent = {
+			enable = true,
+		};
+	},
+}
