02957043cd851623a5274dc658e607308517f9ba
Luis Saturno <saturno.luis@gmail.com>
2024-06-30 01:16:53 +0200

    Add neo-tree and keymaps

--- nvim/lua/config/editor.lua
+++ nvim/lua/config/editor.lua
@@ -1,8 +1,11 @@
+vim.opt.colorcolumn = "+1"
+vim.opt.expandtab = false
+vim.opt.list = true
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.list = true
-
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
-vim.opt.expandtab = false
+vim.opt.smartindent = true
+vim.opt.softtabstop = 4
+vim.opt.tabstop = 4
+vim.opt.textwidth = 80
+
--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -5,9 +5,47 @@ vim.g.mapleader = ' '
 vim.keymap.set('', '<Space>', '<Nop>', {noremap = true, silent = true})
 
 -- Quick jump to start/end of line
-vim.keymap.set("n", "<leader>h", "<home>", {noremap = true, silent = true, desc = "Move cursor to the start of current line."})
-vim.keymap.set("n", "<leader>l", "<End>", {noremap = true, silent = true, desc = "Move cursor to the end of current line." })
-vim.keymap.set("v", "<leader>h", "<Home>", {noremap = true, silent = true, desc = "Select to the start of current line."})
-vim.keymap.set("v", "<leader>l", "<End>", {noremap = true, silent = true, desc = "Select to the end of current line." })
+vim.keymap.set("n", "<leader>h", "<home>", {
+	noremap = true,
+	silent = true,
+	desc = "Move cursor to the start of current line."
+})
 
--- 
+vim.keymap.set("n", "<leader>l", "<End>", {
+	noremap = true,
+	silent = true,
+	desc = "Move cursor to the end of current line."
+})
+
+vim.keymap.set("v", "<leader>h", "<Home>", {
+	noremap = true,
+	silent = true,
+	desc = "Select to the start of current line."
+})
+
+vim.keymap.set("v", "<leader>l", "<End>", {
+	noremap = true,
+	silent = true,
+	desc = "Select to the end of current line."
+})
+
+-- LSP key bindings
+vim.keymap.set("n", "<leader>fd", vim.lsp.buf.definition, {
+	noremap = true,
+	silent = true,
+	desc = "Find symbol definition."
+})
+
+vim.keymap.set("n", "<leader>fi", vim.lsp.buf.implementation, {
+	noremap = true,
+	silent = true,
+	desc = "Find symbol implementation"
+})
+
+-- Open Neotree floating
+vim.keymap.set(
+	"n",
+	"<leader>p",
+	":Neotree reveal<CR>",
+	{ noremap = true, desc = "Reveal buffer in file explorer" }
+)
